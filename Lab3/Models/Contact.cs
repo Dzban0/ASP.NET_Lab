802e8b88a3bb270d5cea1d31ac7a704fec66e3ed
MrKazio <115210775+Dzban0@users.noreply.github.com>
2023-10-29 11:13:07 +0100

    Dodaj pliki projektów.

---
+++ Lab3/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Musisz wpisać imię")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Zbyt długie imię. Maksumalnie 100")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Musisz podać email")]
+        [EmailAddress(ErrorMessage = "Nieporawny adres email")]
+        public string Email { get; set; }
+        [Phone(ErrorMessage = "Niepoprawny numer telefonu, wpisz cyfry")] 
+        public string Phone { get; set; }
+        public DateTime Birth { get; set; }
+
+    }
+}
